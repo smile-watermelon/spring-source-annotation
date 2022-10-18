562778ce9283124400bc30a3943561721d2f4e7b
smile watermelon <18516833610@163.com>
2022-10-18 13:03:47 +0800

    添加DemoApplication 入口查看注释

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # spring-source-annotation
 ```
-概述： 该注释会以spring web demoApplication项目为例，对一个web项目的运行流程进行详细的
-注释
+概述： 该注释会以spring web DemoApplication项目为例，对一个web项目的运行流程进行详细的
+注释, 从DemoApplication 进入可以查看注释
 ```
 - spring-boot 版本 v2.7.4
+- spring-framework 版本 5.3.x
 
---
+++ src/main/java/com/guagua/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.guagua;
+
+import com.guagua.springboot.SpringApplication;
+
+/**
+ * @author guagua
+ * @date 2022/10/18 13:00
+ * @describe 从入口类进入，step by step 看注释哦~
+ */
+
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class,args);
+    }
+}
